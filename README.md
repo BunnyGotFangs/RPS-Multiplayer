35badd164e0e5e482d480b294cdc3e9fdac2847f
BunnygotFangs <kimberly@thehilts.com>
2018-04-27 21:19:42 -0500

    html framework completed; css created; images loaded; firebase initialized; 1st  psuedocode complete

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# RPS-Multiplayer
-Homework 7 Assignment - Rock Paper Scissors (Challenge)
+# Week-7 RPS Multi-Player Game
+JQuery Homework Assignment Week 5
+
+
+WHAT THE PROJECT DOES:
+
+
+
+WHY IT IS USEFUL:
+
+
+
+
+
+START FROM HERE: 
+
+    1. 
+    2. 
+    3. 
+    4.
+    5. 
+    6. 
+    7. 
+    8. 
+
+To get to the game copy the URL below and paste into your browser.
+
+    https://bunnygotfangs.github.io/RPG-multiplayer/
+
+
+NEED HELP or HAVE SUGGESTIONS:
+
+    Have questions or need help with the code, the game mechanics or have suggestions then please feel free to contact me at my website:
+
+    https://bunnygotfangs.github.io/Bootstrap-Portfolio/contact.html
+
+CONTRIBUTIONS:
+
+    Many thanks to all the trailblazers before me who actually got their attack to work right! To those who survived coding BootCamp! -- I truly hope you have landed where you want to be as this is a journey, a sprint, a marathon, and an F&*$'in HILL CLIMB! 
+
+    Thanks for having a sense of humor!
---
+++ assets/javascript/script.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD0yvYEfgwdYNCngkx0cIf-2PaTiPD3d6E",
+    authDomain: "hilttool-9c200.firebaseapp.com",
+    databaseURL: "https://hilttool-9c200.firebaseio.com",
+    projectId: "hilttool-9c200",
+    storageBucket: "hilttool-9c200.appspot.com",
+    messagingSenderId: "468011554717"
+  };
+
+  firebase.initializeApp(config);
+
+  // next need to create variable that are referencing the DB
+
+    var database = firebase.database();
+    //not sure if I need below but reviewed it from classroom work from last week and the following appear to be needed before declaring variables
+    var connectionsRef = database.ref("/connections");
+    var connectedRef = database.ref(".info/connected");
+    connectedRef.on("value", function(snap) 
+    
+    {
+        if (snap.val()) 
+        {
+            var con = connectionsRef.push(true);
+            con.onDisconnect().remove();
+        }
+    });
+        connectionsRef.on("value", function(snap) 
+    {
+        $("#connected-viewers").text(snap.numChildren());
+    });
+//the above makes a connection to the DB, got it, but shouldn;t I need to figure out two connections if two people are playing haven't seen that in class work yet so maybe not
+//define the variables for left side opponent using and array to create a player object
+    var LeftPlayer = {
+        PlayerName: '0',
+        PlayerChoice: '',
+        wins: 0,
+        losses: 0,
+        PlayerSmack: ''
+    };
+//define the variables for right side opponent using an array to create a player object   
+    var RightPlayer = {
+        PlayerName: '0',
+        PlayerChoice: '',
+        wins: 0,
+        losses: 0,
+        PlayerSmack: ''
+    };
+
+//1. need to set-up the variables in the database by creating a snapshot, so DB can relate to variables
+
+//2. Need to input the error handling statement
+}, function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
+ 
+//3. Need an onclick event for capturing Left Player inputs
+//4. Capture left player values into Firebase 
+//5. Need an onclick event for capturing Right Player input
+//6. Capture right player values into Firebase
+//7. Once value are capture display the players choice to screen
+//8. calculate the winner (reference class assignment wk 2/3)
+//9. Display the winner
+//10. onlcick event to capture player chatter
+//11. capture chatter in frebase
+//12. call firebase and append jumbtron text with player smack
+//13. need to clear the players
+//14. need to reset the score
